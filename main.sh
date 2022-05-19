b8be99d10537b68e853d03fa7c952c4fedcbbfa3
Bruce CHEN <805899926@.com>
2022-05-19 10:22:28 +0800

    #

--- main.sh
+++ main.sh
@@ -2,26 +2,26 @@
 
 # lede luci
 for i in "applications/luci-app-softethervpn" "applications/luci-app-vlmcsd" "applications/luci-app-adbyby-plus"; do
-  svn export --force "https://github.com/coolsnowwolf/luci/trunk/$i"
+  svn export --force "https://github.com/coolsnowwolf/luci/trunk/$i" app/$(basename $i)
 done
 
 # lede packages
 for i in "net/vlmcsd"; do
-  svn export --force "https://github.com/coolsnowwolf/packages/trunk/$i"
+  svn export --force "https://github.com/coolsnowwolf/packages/trunk/$i" package/$(basename $i)
 done
 
 # lede package
 for i in "package/lean/adbyby"; do
-  svn export --force "https://github.com/coolsnowwolf/lede/trunk/$i"
+  svn export --force "https://github.com/coolsnowwolf/lede/trunk/$i" package/$(basename $i)
 done
 
 # hello packages
 for i in "net/dns2socks" "net/microsocks" "net/ipt2socks" "net/pdnsd-alt" "net/redsocks2"; do
-  svn export --force "https://github.com/immortalwrt/packages/trunk/$i"
+  svn export --force "https://github.com/immortalwrt/packages/trunk/$i" package/$(basename $i)
 done
 
 # custom
-svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/ddns-scripts_aliyun/update_aliyun_com.sh ddns-scripts-aliyun/files/
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/ddns-scripts_aliyun/update_aliyun_com.sh package/ddns-scripts-aliyun/files/
 
 find -name Makefile -exec sed -i "s,include ../../luci.mk,include $\(TOPDIR\)/feeds/luci/luci.mk,g" {} \;
 
