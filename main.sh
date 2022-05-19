3c026229ae5f18b0ddd12524d1d3350b3fb676a1
Bruce CHEN <805899926@.com>
2022-05-19 09:19:46 +0800

    # add adbyby plus

--- main.sh
+++ main.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-for i in "applications/luci-app-softethervpn" "applications/luci-app-vlmcsd"; do
+# lede luci
+for i in "applications/luci-app-softethervpn" "applications/luci-app-vlmcsd" "applications/luci-app-adbyby-plus"; do
   svn export --force "https://github.com/coolsnowwolf/luci/trunk/$i"
 done
 
-svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/ddns-scripts_aliyun/update_aliyun_com.sh ddns-scripts-aliyun/files/
+# lede packages
+for i in "net/vlmcsd"; do
+  svn export --force "https://github.com/coolsnowwolf/packages/trunk/$i"
+done
 
+# lede package
+for i in "package/lean/adbyby"; do
+  svn export --force "https://github.com/coolsnowwolf/lede/trunk/$i"
+done
 
+# hello packages
 for i in "net/dns2socks" "net/microsocks" "net/ipt2socks" "net/pdnsd-alt" "net/redsocks2"; do
   svn export --force "https://github.com/immortalwrt/packages/trunk/$i"
 done
 
-for i in "net/vlmcsd"; do
-  svn export --force "https://github.com/coolsnowwolf/packages/trunk/$i"
-done
+# custom
+svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/ddns-scripts_aliyun/update_aliyun_com.sh ddns-scripts-aliyun/files/
 
 find -name Makefile -exec sed -i "s,include ../../luci.mk,include $\(TOPDIR\)/feeds/luci/luci.mk,g" {} \;
 
