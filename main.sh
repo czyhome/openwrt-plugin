6bff975e59b4f11973a38f711cd35deb8a4f2487
Bruce CHEN <805899926@qq.com>
2022-05-22 09:31:02 +0800

    no message

--- main.sh
+++ main.sh
@@ -23,9 +23,7 @@ done
 # custom
 svn export --force https://github.com/coolsnowwolf/lede/trunk/package/lean/ddns-scripts_aliyun/update_aliyun_com.sh package/ddns-scripts-aliyun/files/
 
-find -name 'Makefile' -exec sed -i "s,include ../../luci.mk,include $\(TOPDIR\)/feeds/luci/luci.mk,g" {} \;
-
-sleep 3
+find -name 'Makefile' -type f -exec sed -i "s,include ../../luci.mk,include $\(TOPDIR\)/feeds/luci/luci.mk,g" {} \;
 
 for i in $(find -name 'zh-cn' -type d); do
   zh_Hans_dir=$(dirname $i)/zh_Hans
