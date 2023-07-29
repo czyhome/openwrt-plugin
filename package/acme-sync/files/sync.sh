c6d655f6f3e7db40a78e1224094046121e993c31
Bruce CHEN <805899926@qq.com>
2023-07-29 15:33:12 +0800

    # fix cp bug

--- package/acme-sync/files/sync.sh
+++ package/acme-sync/files/sync.sh
@@ -17,7 +17,7 @@ do
                   domains=$OPTARG
                         ;;
     *)
-      echo "Usage: $(basename $0) [-s argument] [-t argument]"
+      logger -t "$LOG_TAG" "Usage: $(basename $0) [-s argument] [-t argument]"
       exit 1
       ;;
     esac
@@ -25,13 +25,13 @@ done
 domains=(`echo $domains | tr ',' ' '`)
 
 for i in "${domains[@]}"; do
+  logger -t "$LOG_TAG" "syncing ${i}"
   source_cer=${source_dir}/${i}/$i.cer
   target_cer=${target_dir}/${i}.cer
   if [ -f "${source_cer}" ];then
     if [ ! -f ${target_cer} ] || [ `md5sum ${source_cer} | awk '{print $1}'` = `md5sum ${target_cer} | awk '{print $1}'` ];then
-      mkdir -p ${target_dir}
-      lo
-      find ${source_dir}/$i -name "$i.cer" -o -name "$i.key" -exec cp {} ${target_dir} \;
+      find ${source_dir}/$i -name "$i.cer" -o -name "$i.key" -exec sh -c 'f={};logger -t "$LOG_TAG" "$(readlink -f $f)";cp $f ${target_dir}/' \;
+      logger -t "$LOG_TAG" "synced ${i}"
     fi
   fi
 done
