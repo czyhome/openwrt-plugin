369e7c63239ff6a74b8c87de886aea16cb76733b
Bruce CHEN <805899926@qq.com>
2023-07-29 18:49:04 +0800

    # improve log_tag

--- package/acme-sync/files/sync.sh
+++ package/acme-sync/files/sync.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # example cron: 0 0 * * * LOG_TAG=acme-sync /etc/acme/sync.sh -s <source_dir> -t <target_dir> -d example.com -p "echo 'post_shell'"
+LOG_TAG=${LOG_TAG-"acme-sync"}
 
 source_dir=
 target_dir=
@@ -34,7 +35,7 @@ for i in "${domains[@]}"; do
   logger -t "$LOG_TAG" "${i}"
   source_cer=${source_dir}/${i}/$i.cer
   target_cer=${target_dir}/${i}.cer
-  if [[ -f ${source_cer} && ! -f ${target_cer} || -f ${source_cer} && -f ${target_cer} && ! -z "$(diff -q ${source_cer} ${target_cer})" ]];then
+  if [[ -f ${source_cer} && ! -f ${target_cer} || -f ${source_cer} && -f ${target_cer} && ! -z `diff -q ${source_cer} ${target_cer}` ]];then
     for f in `find ${source_dir}/$i -name "$i.cer" -o -name "$i.key"`;do
       logger -t "$LOG_TAG" "$(readlink -f $f) ->  ${target_dir}";
       cp $f ${target_dir}
