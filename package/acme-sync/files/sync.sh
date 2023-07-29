81b7412e8d5da74fb00e07c84f4cc9b4f4de579a
Bruce CHEN <805899926@qq.com>
2023-07-29 15:33:44 +0800

    #

--- package/acme-sync/files/sync.sh
+++ package/acme-sync/files/sync.sh
@@ -29,7 +29,7 @@ for i in "${domains[@]}"; do
   source_cer=${source_dir}/${i}/$i.cer
   target_cer=${target_dir}/${i}.cer
   if [ -f "${source_cer}" ];then
-    if [ ! -f ${target_cer} ] || [ `md5sum ${source_cer} | awk '{print $1}'` = `md5sum ${target_cer} | awk '{print $1}'` ];then
+    if [ ! -f ${target_cer} ] || [ `md5sum ${source_cer} | awk '{print $1}'` != `md5sum ${target_cer} | awk '{print $1}'` ];then
       find ${source_dir}/$i -name "$i.cer" -o -name "$i.key" -exec sh -c 'f={};logger -t "$LOG_TAG" "$(readlink -f $f)";cp $f ${target_dir}/' \;
       logger -t "$LOG_TAG" "synced ${i}"
     fi
