144aa987ed403da1758b7d5b2675c96a1675eb15
Bruce CHEN <805899926@qq.com>
2023-08-24 22:30:02 +0800

    # luci-app-dnsproxy finished list

---
+++ luci/luci-app-dnsproxy/luasrc/controller/dnsproxy.lua
@@ -0,0 +1,8 @@
+-- Copyright 2023 Steven Barth <a805899926@gmail.com>
+-- Licensed to the public under the Apache License 2.0.
+
+module("luci.controller.dnsproxy", package.seeall)
+
+function index()
+	entry( {"admin", "services", "dnsproxy"}, cbi("dnsproxy"), _("DnsProxy") ).acl_depends = { "luci-app-dnsproxy" }
+end
---
+++ luci/luci-app-dnsproxy/luasrc/model/cbi/dnsproxy.lua
@@ -0,0 +1,77 @@
+-- Copyright 2023 Steven Barth <a805899926@gmail.com>
+-- Licensed to the public under the Apache License 2.0.
+
+local fs  = require "nixio.fs"
+local sys = require "luci.sys"
+local uci = require "luci.model.uci".cursor()
+local testfullps = sys.exec("ps --help 2>&1 | grep BusyBox") --check which ps do we have
+local psstring = (string.len(testfullps)>0) and  "ps w" or  "ps axfw" --set command we use to get pid
+
+local m = Map("dnsproxy", translate("DnsProxy"))
+local s = m:section( TypedSection, "dnsproxy", translate("DnsProxy instances"), translate("Below is a list of configured DnsProxy instances and their current state") )
+s.template = "cbi/tblsection"
+s.addremove = true
+s.add_select_options = { }
+
+function s.getPID(section) -- Universal function which returns valid pid # or nil
+	local pid = sys.exec("%s | grep -w '[d]nsproxy *.%s.log'" % { psstring, section })
+	if pid and #pid > 0 then
+		return tonumber(pid:match("^%s*(%d+)"))
+	else
+		return nil
+	end
+end
+
+function s.remove(self, name)
+	uci:delete("dnsproxy", name)
+	uci:save("dnsproxy")
+	uci:commit("dnsproxy")
+end
+
+s:option( Flag, "enabled", translate("Enabled") )
+
+local active = s:option( DummyValue, "_active", translate("Started") )
+function active.cfgvalue(self, section)
+	local pid = s.getPID(section)
+	if pid ~= nil then
+		return (sys.process.signal(pid, 0))
+			and translatef("yes (%i)", pid)
+			or  translate("no")
+	end
+	return translate("no")
+end
+
+-- local updown = s:option( Button, "_updown", translate("Start/Stop") )
+-- updown._state = false
+-- updown.redirect = luci.dispatcher.build_url("admin", "services", "dnsproxy")
+
+-- function updown.cbid(self, section)
+-- 	local pid = s.getPID(section)
+-- 	self._state = pid ~= nil and sys.process.signal(pid, 0)
+-- 	self.option = self._state and "stop" or "start"
+-- 	return AbstractValue.cbid(self, section)
+-- end
+-- function updown.cfgvalue(self, section)
+-- 	self.title = self._state and "stop" or "start"
+-- 	self.inputstyle = self._state and "reset" or "reload"
+-- end
+-- function updown.write(self, section, value)
+-- 	if self.option == "stop" then
+-- 		sys.call("/etc/init.d/dnsproxy stop %s" % section)
+-- 	else
+-- 		sys.call("/etc/init.d/dnsproxy start %s" % section)
+-- 	end
+-- 	luci.http.redirect( self.redirect )
+-- end
+
+local port = s:option( DummyValue, "listen_port", translate("Port") )
+function port.cfgvalue(self, section)
+	local val = AbstractValue.cfgvalue(self, section)
+	return val or "-"
+end
+
+function m.on_after_apply(self,map)
+	sys.call('/etc/init.d/dnsproxy reload')
+end
+
+return m
