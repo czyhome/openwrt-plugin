d62bf9e3e6c3ce9eaa497348fac3e4a4479c347f
Bruce CHEN <805899926@qq.com>
2023-08-29 21:06:54 +0800

    #

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd $(cd "$(dirname "$0")"; pwd)
-cp -rv package/dnsproxy/* ../packages/dnsproxy/
-cp -rv package/adguardhome/* ../packages/adguardhome/
-sed -i 's|/etc/adguardhome.yaml|/etc/adguardhome/|' ../package/adguardhome/Makefile
+cp -rv package/dnsproxy/* ../packages/net/dnsproxy/
+cp -rv package/adguardhome/* ../packages/net/adguardhome/
+sed -i 's|/etc/adguardhome.yaml|/etc/adguardhome/|' ../packages/net/adguardhome/Makefile
