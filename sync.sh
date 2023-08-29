aeafae25e84c0c3e22a107eabf45be00f7f94e1c
Bruce CHEN <805899926@qq.com>
2023-08-29 22:03:42 +0800

    #

--- sync.sh
+++ sync.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 cd $(cd "$(dirname "$0")"; pwd)
+
+# dnsproxy
 cp -rv package/dnsproxy/* ../packages/net/dnsproxy/
+sed -i -e 's|\sUSERID:=dnsproxy=411.*||' -e '/^define Package\/dnsproxy\/conffiles/,/^endef$/d' ../packages/net/dnsproxy/Makefile
+call_line=`grep -n '$(eval $(call GoBinPackage,dnsproxy))' feeds/packages/net/dnsproxy/Makefile | cut -d ":" -f 1`
+sed -i -e "$call_line idefine Package/dnsproxy/conffiles\n/etc/config/dnsproxy\n/etc/dnsproxy/\nendef"
+# adguardhome
 cp -rv package/adguardhome/* ../packages/net/adguardhome/
-sed -i 's|/etc/adguardhome.yaml|/etc/adguardhome/|' ../packages/net/adguardhome/Makefile
+sed -e 's|\sUSERID:=dnsproxy=411.*||' -e "/^define Package\/dnsproxy\/conffiles/,/^endef$/d" ../packages/net/adguardhome/Makefile
