268c079fe08a9fc17edf9aa4aaca363fb2c3b1d5
Bruce CHEN <805899926@qq.com>
2023-08-30 21:48:32 +0800

    #

--- sync.sh
+++ sync.sh
@@ -8,12 +8,9 @@ function cp_pkg(){
     [ -f "package/${pkg_name}/Makefile.override" ] && cp -rv package/${pkg_name}/Makefile.override ../packages/${pkg_path}/Makefile    
 }
 
-# dnsproxy
-cp_pkg net/dnsproxy
-
-# adguardhome
-cp_pkg net/adguardhome
+for i in "net/dnsproxy" "net/adguardhome" "libs/openldap"; do
+  cp_pkg $i
+done
 
 # openldap
-cp_pkg libs/openldap
 sed -i -e 's|$(INSTALL_BIN) ./files/ldap.init $(1)/etc/init.d/ldap|$(INSTALL_BIN) ./files/openldap.init $(1)/etc/init.d/openldap|' ../packages/libs/openldap/Makefile
