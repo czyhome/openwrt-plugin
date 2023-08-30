6591a51c32bcc15d4851bf318197dfd552815e0b
Bruce CHEN <805899926@qq.com>
2023-08-30 09:43:40 +0800

    #

--- sync.sh
+++ sync.sh
@@ -6,7 +6,7 @@ function replace_conffiles(){
     local pkg_makefile=$2
     local before_line=$3
     sed -i -e "/^define Package\/${pkg_name}\/conffiles/,/^endef$/d" $pkg_makefile
-    sed -i -e "`grep -n "${before_line}" $pkg_makefile | cut -d ":" -f 1` idefine Package/${pkg_name}/conffiles\n/etc/config/${pkg_name}\n/etc/${pkg_name}/\nendef" $pkg_makefile
+    sed -i -e "`grep -n "${before_line}" $pkg_makefile | cut -d ":" -f 1`i define Package/${pkg_name}/conffiles\n/etc/config/${pkg_name}\n/etc/${pkg_name}/\nendef" $pkg_makefile
 }
 
 # dnsproxy
@@ -19,3 +19,4 @@ replace_conffiles dnsproxy $dnsproxy_makefile '$(eval $(call GoBinPackage,dnspro
 cp -rv package/adguardhome/* ../packages/net/adguardhome/
 adguardhome_makefile=../packages/net/adguardhome/Makefile
 replace_conffiles adguardhome $adguardhome_makefile '$(eval $(call GoBinPackage,adguardhome))'
+grep -q "\$(INSTALL_DIR) \$(1)/etc/adguardhome" $adguardhome_makefile || sed -i -e "`grep -n '$(call GoPackage/Package/Install/Bin,$(1))' $adguardhome_makefile | cut -d ":" -f 1`a\ \t\$(INSTALL_DIR) \$(1)/etc/adguardhome" $adguardhome_makefile
