6403d0ce6fcfb0c65d66a34a80624076b010a43e
Bruce CHEN <805899926@qq.com>
2023-08-30 21:22:25 +0800

    # improve openldap.init

--- sync.sh
+++ sync.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 cd $(cd "$(dirname "$0")"; pwd)
 
+function cp_pkg(){
+    pkg_path=$1
+    pkg_name=`basename $1`
+    cp -rv package/${pkg_name}/files/ ../packages/${pkg_path}
+    [ -f "package/${pkg_name}/Makefile.override" ] && cp -rv package/${pkg_name}/Makefile.override ../packages/${pkg_path}/Makefile    
+}
+
 # dnsproxy
-cp -rv package/dnsproxy/files/ ../packages/net/dnsproxy/
-cp -rv package/dnsproxy/Makefile.override ../packages/net/dnsproxy/Makefile
+cp_pkg net/dnsproxy
 
 # adguardhome
-cp -rv package/adguardhome/files/ ../packages/net/adguardhome/
-cp -rv package/adguardhome/Makefile.override ../packages/net/adguardhome/Makefile
+cp_pkg net/adguardhome
+
+# openldap
+cp_pkg openldap libs/openldap
+sed -i -e 's|$(INSTALL_BIN) ./files/ldap.init $(1)/etc/init.d/ldap|$(INSTALL_BIN) ./files/openldap.init $(1)/etc/init.d/openldap|' ../packages/libs/openldap/Makefile
