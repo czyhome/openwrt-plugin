b4964a78afe797b7604d5c504983abf9d23c7bc9
Bruce CHEN <805899926@qq.com>
2023-09-04 14:25:59 +0800

    #

--- function/get_targets.py
+++ function/get_targets.py
@@ -11,9 +11,12 @@ for t in pathlib.Path(__file__).parent.parent.joinpath(".github/workflows").rglo
     with io.open(t, "r", encoding="utf8") as f:
         obj: dict = yaml.full_load(f)
         target = obj.get("jobs").get("build_ext").get("with").get("target")
-        output_image_type=obj.get("jobs").get("build_ext").get("with").get("output_image_type","")
+        output_image_type = obj.get("jobs").get("build_ext").get("with").get("output_image_type")
         targets.append(target)
         for sub in obj.get("jobs").get("build_ext").get("strategy").get("matrix").get("subtarget"):
-            subtargets.append({"target": target, "subtarget": sub,"output_image_type":output_image_type})
+            st = {"target": target, "subtarget": sub}
+            if output_image_type:
+                st["output_image_type"] = output_image_type
+            subtargets.append(st)
 print("targets={0}".format(json.dumps(targets)))
 print("targets_subtargets={0}".format(json.dumps(subtargets)))
