7862bed00cc9e1d7b26383c233272ce88230a96c
czy21 <805899926@qq.com>
2024-03-16 01:33:19 +0800

    更新 main.sh

--- config/clash/main.sh
+++ config/clash/main.sh
@@ -4,4 +4,4 @@ cd $(cd "$(dirname "$0")"; pwd)
 # rm -rf metatube-sdk-go
 # git clone https://github.com/metatube-community/metatube-sdk-go.git
 
-find ./metatube-sdk-go/provider -name '*.go' -exec sh -c 'sed -n "s|baseURL\(.*\)\"\(.*\)\"|\2|p" {} | cut -d"/" -f3 | sed -e "s|/.*$||g" -re "s|^.*\.([^\.]+\.[^\.]+$)|\1|g" -e "/github/d"'  \; | sort | uniq | sed -e "s|^|DOMAIN-SUFFIX,|" > MetaTube.list
+find ./metatube-sdk-go/provider -name '*.go' -exec sh -c 'sed -n "s|baseURL\(.*\)\"\(.*\)\"|\2|p" {} | cut -d"/" -f3 | sed -re "s|^.*\.([^\.]+\.[^\.]+$)|\1|g" -e "/github/d"'  \; | sort | uniq | sed -e "s|^|DOMAIN-SUFFIX,|" > MetaTube.list
