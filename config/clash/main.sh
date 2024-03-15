3d525c7380e2374c3152a34cddb66db3a620bab5
Bruce CHEN <805899926@qq.com>
2024-03-15 19:33:30 +0800

    # remove MetaTube

--- config/clash/main.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd $(cd "$(dirname "$0")"; pwd)
-
-rm -rf metatube-sdk-go
-git clone https://github.com/metatube-community/metatube-sdk-go.git
-
-find ./metatube-sdk-go/provider -name '*.go' -exec sh -c 'sed -n "s|baseURL\(.*\)\"\(.*\)\"|\2|p" {} | cut -d"/" -f3 | sed -e "s|^|DOMAIN-SUFFIX,|"'  \; > MetaTube.list
