989308dd15c01cc5fd724c57a8d350112584942c
Bruce CHEN <805899926@qq.com>
2024-03-16 14:37:52 +0800

    # add metatube rule

--- config/clash/main.sh
+++ config/clash/main.sh
@@ -4,4 +4,4 @@ cd $(cd "$(dirname "$0")"; pwd)
 # rm -rf metatube-sdk-go
 # git clone https://github.com/metatube-community/metatube-sdk-go.git
 
-find ./metatube-sdk-go/provider -name '*.go' -exec sh -c 'sed -n "s|baseURL\(.*\)\"\(.*\)\"|\2|p" {} | cut -d"/" -f3 | sed -re "s|^.*\.([^\.]+\.[^\.]+$)|\1|g" -e "/github/d"'  \; | sort | uniq | sed -e "s|^|DOMAIN-SUFFIX,|" > MetaTube.list
+find ./metatube-sdk-go/provider -name '*.go' -exec sh -c 'sed -n "s|baseURL\(.*\)\"\(.*\)\"|\2|p" {} | cut -d"/" -f3' \; | awk '{ split($0, arr, "."); if (length(arr)>2) {print substr($0,length(arr[1])+2)} else {print $0}; }' | sort | uniq | sed -e "s|^|DOMAIN-SUFFIX,|" -e "/github/d" > MetaTube.list
