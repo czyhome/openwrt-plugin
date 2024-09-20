86cd85315cf5b53f9d503f727e530d00e85c6048
Bruce CHEN <805899926@qq.com>
2024-09-20 21:57:42 +0800

    #

--- .github/workflows/scripts/get_targets.py
+++ .github/workflows/scripts/get_targets.py
@@ -4,6 +4,7 @@ import pathlib
 import argparse
 import yaml
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--openwrt-master', action="store_true")
 args: argparse.Namespace = parser.parse_args()
@@ -16,7 +17,7 @@ for t in pathlib.Path(__file__).parent.parent.rglob("target-*.yml"):
         target = obj.get("jobs").get("build_ext").get("with").get("target")
         output_image_type = obj.get("jobs").get("build_ext").get("with").get("output_image_type")
         targets.append(target)
-        for sub in obj.get("env").get("{}_subtarget".format('master' if args.openwrt_master else 'stable')).split(","):
+        for sub in obj.get("env").get("build_ext").get("strategy").get("matrix").get("{}_subtarget".format('master' if args.openwrt_master else 'stable')):
             st = {"target": target, "subtarget": sub}
             if output_image_type:
                 st["output_image_type"] = output_image_type
