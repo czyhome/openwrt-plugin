319a725c6636b47d399d3d70c808fa0783e98a44
Bruce CHEN <805899926@qq.com>
2024-09-22 20:10:39 +0800

    # add bcm27xx

--- .github/workflows/scripts/get_targets.py
+++ .github/workflows/scripts/get_targets.py
@@ -4,7 +4,6 @@ import pathlib
 import argparse
 import yaml
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--openwrt-master', action="store_true")
 args: argparse.Namespace = parser.parse_args()
@@ -13,12 +12,14 @@ targets = []
 subtargets = []
 for t in pathlib.Path(__file__).parent.parent.rglob("target-*.yml"):
     with io.open(t, "r", encoding="utf8") as f:
-        obj: dict = yaml.full_load(f)
-        target = obj.get("jobs").get("build_ext").get("with").get("target")
-        output_image_type = obj.get("jobs").get("build_ext").get("with").get("output_image_type","")
-        targets.append(target)
-        for sub in obj.get("jobs").get("build_ext").get("strategy").get("matrix").get("{}_subtarget".format('master' if args.openwrt_master else 'stable'),[]):
-            st = {"target": target, "subtarget": sub,"output_image_type":output_image_type}
-            subtargets.append(st)
+        target_obj: dict = yaml.full_load(f)
+        build_ext = target_obj.get("jobs").get("build_ext")
+        target = build_ext.get("with").get("target")
+        output_image_type = build_ext.get("with").get("output_image_type","")
+        target_subtargets=[{"target": target, "subtarget": st,"output_image_type":output_image_type} for st in build_ext.get("strategy").get("matrix").get("{}_subtarget".format('master' if args.openwrt_master else 'stable'),[])]
+        subtargets.extend(target_subtargets)
+        if target_subtargets:
+            targets.append(target)
+
 print("targets={0}".format(json.dumps(targets)))
 print("targets_subtargets={0}".format(json.dumps(subtargets)))
