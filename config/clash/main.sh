09aa6ac9a023e2a24144d9103907de7c4ea8bcbe
Bruce CHEN <805899926@qq.com>
2024-09-23 13:44:26 +0800

    # improve workflow

--- config/clash/main.sh
+++ config/clash/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd $(cd "$(dirname "$0")"; pwd)
 
-# rm -rf metatube-sdk-go
-# git clone https://github.com/metatube-community/metatube-sdk-go.git
+[ -d "metatube-sdk-go/.git" ] || git clone https://github.com/metatube-community/metatube-sdk-go.git
+(cd metatube-sdk-go;git pull)
 
 find ./metatube-sdk-go/provider -name '*.go' -exec sh -c 'sed -n "s|baseURL\(.*\)\"\(.*\)\"|\2|p" {} | cut -d"/" -f3' \; | awk '{ split($0, arr, "."); if (length(arr)>2) {print substr($0,length(arr[1])+2)} else {print $0}; }' | sort | uniq | sed -e "s|^|DOMAIN-SUFFIX,|" -e "/github/d" > MetaTube.list
