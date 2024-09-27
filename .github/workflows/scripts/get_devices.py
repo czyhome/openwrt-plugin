869a8e20bd798359640bc4b3354bd2de2c81eb44
Bruce CHEN <805899926@qq.com>
2024-09-27 13:19:57 +0800

    # improve subtarget to device

--- .github/workflows/scripts/get_devices.py
+++ .github/workflows/scripts/get_devices.py
@@ -9,17 +9,17 @@ parser.add_argument('--openwrt-master', action="store_true")
 args: argparse.Namespace = parser.parse_args()
 
 targets = []
-subtargets = []
+devices = []
 for t in pathlib.Path(__file__).parent.parent.rglob("target-*.yml"):
     with io.open(t, "r", encoding="utf8") as f:
         target_obj: dict = yaml.full_load(f)
         build_ext = target_obj.get("jobs").get("build_ext")
         target = build_ext.get("with").get("target")
-        output_image_type = build_ext.get("with").get("output_image_type","")
-        target_subtargets=[{"target": target, "subtarget": st,"output_image_type":output_image_type} for st in build_ext.get("strategy").get("matrix").get("{}_subtarget".format('master' if args.openwrt_master else 'stable'),[])]
-        subtargets.extend(target_subtargets)
-        if target_subtargets:
+        device_image_type = build_ext.get("with").get("device_image_type","")
+        target_devices=[{"target": target, "device": st,"device_image_type":device_image_type} for st in build_ext.get("strategy").get("matrix").get("{}_device".format('master' if args.openwrt_master else 'stable'),[])]
+        devices.extend(target_devices)
+        if target_devices:
             targets.append(target)
 
 print("targets={0}".format(json.dumps(targets)))
-print("targets_subtargets={0}".format(json.dumps(subtargets)))
+print("targets_devices={0}".format(json.dumps(devices)))
