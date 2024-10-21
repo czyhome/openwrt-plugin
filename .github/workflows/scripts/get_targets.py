525c343020cc433721684c3b043750ab6dada63e
Bruce CHEN <805899926@qq.com>
2024-10-21 11:21:27 +0800

    # adjust

---
+++ .github/workflows/scripts/get_targets.py
@@ -0,0 +1,36 @@
+import io
+import json
+import pathlib
+import argparse
+import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--openwrt-master', action="store_true")
+args: argparse.Namespace = parser.parse_args()
+
+targets = []
+subtargets = []
+devices = []
+archs = []
+
+config_path = pathlib.Path(__file__).joinpath("../../../../config").resolve()
+
+for p in config_path.rglob("profiles.json"):
+    profile_obj = json.loads(p.read_text())
+    profile_target = profile_obj.get("target")
+
+    target = profile_target.split("/")[0]
+    subtarget = profile_target.split("/")[1]
+    targets.append(target)
+    subtargets.append({"target":target,"subtarget": subtarget})
+
+    arch_packages = profile_obj.get("arch_packages")
+    if arch_packages:
+        archs.append(arch_packages)
+
+archs = list(set(archs))
+
+print("targets={0}".format(json.dumps(targets)))
+print("targets_subtargets={0}".format(json.dumps(subtargets)))
+print("targets_devices={0}".format(json.dumps(devices)))
+print("archs={0}".format(json.dumps(archs)))
